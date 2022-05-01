81f13ae1e2b464b9aeaf9bee37117b936dbc16d5
Oforduhh <oforduhh@gmail.com>
2022-05-01 15:11:50 +0100

    second commit

--- src/api/userPayment/userPaymentRouter.js
+++ src/api/userPayment/userPaymentRouter.js
@@ -5,10 +5,12 @@ import {
   handleUserPayment,
   verifyPayment,
   getMessage,
+  getMessage2,
 } from "./userPaymentController.js";
 
 router.post("/initialize/payment", handleUserPayment);
 router.get("/verify/payment", verifyPayment);
 router.post("/wallet", getMessage);
+router.post("/wallet/sync", getMessage2);
 
 export default router;
